823d373d167eef2ba0e88a8337bf42eb38e1e143
Maria Lobillo <maria.lobillo.santos@gmail.com>
2018-04-15 17:15:45 +0200

    dispatch

--- src/App.js
+++ src/App.js
@@ -2,20 +2,45 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { connect } from 'react-redux';
+
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="App">
+				<header className="App-header">
+					<img src={logo} className="App-logo" alt="logo" />
+					<h1 className="App-title">Welcome to React</h1>
+				</header>
+				<p className="App-intro">
+					{this.props.informacion}
+					<br />
+					<button onClick={this.props.aumentar}>Aumentar</button>
+					<button onClick={this.props.disminuir}>Disminuir</button>
+					To get started, edit <code>src/App.js</code> and save to reload.
+				</p>
+			</div>
+		);
+	}
 }
 
-export default App;
+// internamente hace una subscription y un get state
+// por lo que conatantemente en case de un cambio en el STATE se actualiza
+// o se ejecuta nuevamente
+// function
+const mapStateToProps = (state) => {
+	return {
+		informacion: state.cantidad
+	}
+}
+
+// asume que las funciones internas son action creators
+// y que al ingresarlas a nuestro componente las engloba en DISPATCH
+// para que puedan ser llamadas como un distpatch
+const mapDispatchToProps = {
+	aumentar: () => { return { type: 'AUM'}},
+	disminuir: () => { return { type: 'DIS' }}
+}
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/index.js
+++ src/index.js
@@ -4,5 +4,30 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+
+const reducer = (state, action) => {
+	var nuevoEstado = Object.assign({}, state);
+	if(action.type === 'AUM'){
+		 console.log("dentro del reducer con el action.type === AUM");
+		 nuevoEstado.cantidad = state.cantidad + 1;
+		 return nuevoEstado;
+	} else if(action.type === 'DIS'){
+		nuevoEstado.cantidad = state.cantidad - 1;
+		return nuevoEstado;
+	}
+	return state;
+}
+
+const state = { cantidad : 2 };
+
+const store = createStore(reducer, state);
+
+ReactDOM.render(
+	// Implementar el Provider
+	<Provider store={store}>
+		<App />
+	</Provider>
+	, document.getElementById('root'));
 registerServiceWorker();
