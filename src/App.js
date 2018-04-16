251a566057f2ff5d86a2a4f8f7b8546ddf7361e4
Maria Lobillo <maria.lobillo.santos@gmail.com>
2018-04-16 17:32:27 +0200

    redux thunk

--- src/App.js
+++ src/App.js
@@ -8,17 +8,20 @@ class App extends Component {
 	agregarTarea = (evento) => {
 		if(evento.which === 13){
 			console.log(evento.target.value);
-			this.props.agregar(evento.target.value, 1);
+			this.props.agregar(evento.target.value, this.props.id);
 		}
 	}
 	render() {
+		const elementosTareas = this.props.tareas.map((tarea) => {
+			return <h2 key={tarea.id}>{tarea.tarea}</h2>;
+		})
 		return (
 			<div className="App">
 				<header className="App-header">
 					<img src={logo} className="App-logo" alt="logo" />
 					<h1 className="App-title">Welcome to React</h1>
 				</header>
-				<p className="App-intro">
+				<div className="App-intro">
 					{this.props.informacion}
 					<br />
 					<button onClick={this.props.aumentar}>Aumentar</button>
@@ -26,8 +29,10 @@ class App extends Component {
 					<br />
 					<input type="text" onKeyPress={this.agregarTarea.bind(this)} id=""/>
 					<br />
+					{elementosTareas}
+					<br />
 					To get started, edit <code>src/App.js</code> and save to reload.
-				</p>
+				</div>
 			</div>
 		);
 	}
@@ -39,7 +44,9 @@ class App extends Component {
 // function
 const mapStateToProps = (state) => {
 	return {
-		informacion: state.numero
+		informacion: state.numero,
+		tareas: state.tareas,
+		id: state.id
 	}
 }
 
@@ -48,8 +55,14 @@ const mapStateToProps = (state) => {
 // para que puedan ser llamadas como un distpatch
 const mapDispatchToProps = (dispatch) => {
 	return {
-		aumentar: () => { dispatch( { type: 'AUM'} ); },
+		aumentar: () => { dispatch(
+			(dispatch) => {
+				return dispatch( { type: 'AUM'} )
+			}
+		); },
 		disminuir: () => { dispatch({ type: 'DIS' }); },
+		// disminuir: () => { dispatch({ type: 'DIS' }); },
+
 		agregar: (tarea, id) => { dispatch( { type: 'ADD', tarea, id}); }
 	}
 }
--- src/index.js
+++ src/index.js
@@ -4,8 +4,10 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-import {createStore, combineReducers} from 'redux';
+import {createStore, combineReducers, applyMiddleware } from 'redux';
 import {Provider} from 'react-redux';
+
+import ReduxThunk from 'redux-thunk';
 
 const reducerNumero = (state = 2, action) => {
 	var nuevoEstado = Object.assign({}, state);
@@ -31,15 +33,25 @@ const reducerTareas = (state = [], action) => {
 	return state;
 }
 
+const reducerId = (state = 1, action) => {
+	var nuevoEstado = Object.assign({}, state);
+	if(action.type == 'ADD'){
+		nuevoEstado = state + 1;
+		return nuevoEstado;
+	}
+	return state;
+}
+
 // combineReducers toma un objeto JS con los demas reducers
 const reducer = combineReducers({
 	numero: reducerNumero,
-	tareas: reducerTareas
+	tareas: reducerTareas,
+	id: reducerId
 });
 
-const state = { cantidad : 2 };
+const state = 2 ;
 
-const store = createStore(reducer, state);
+const store = createStore(reducer, applyMiddleware(ReduxThunk));
 
 ReactDOM.render(
 	// Implementar el Provider
